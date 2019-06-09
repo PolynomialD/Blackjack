2b00451aa454e903f32aea2fcb12698e0486af9d
polynomiald <steven.grindey@gmail.com>
2019-06-09 17:14:29 +0100

    refactored

--- src/PlayBlackJack.js
+++ src/PlayBlackJack.js
@@ -2,10 +2,11 @@ const Player = require('./Player')
 
 const players = []
 function addNewPlayer() {
-  const name = document.getElementById('nameInput').value
-  const chips = document.getElementById('chipsInput').value
-  const player = new Player(name, chips)
-
+  const name = document.getElementById('nameInput')
+  const chips = document.getElementById('chipsInput')
+  const player = new Player(name.value, chips.value)
+  name.value = ''
+  chips.value = ''
   players.push(player)
   const li = document.createElement('li')
   const nameNode = document.createTextNode(`${player.getName()}`)
