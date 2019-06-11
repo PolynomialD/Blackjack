2dd782b462304f2be82fa5728d51f3bc07e3e14b
polynomiald <steven.grindey@gmail.com>
2019-06-11 10:08:33 +0100

    setUpTable()

--- src/PlayBlackJack.js
+++ src/PlayBlackJack.js
@@ -1,6 +1,7 @@
 const Player = require('./Player')
-
+const BlackJackGame = require('./BlackJackGame.js')
 const players = []
+
 function addNewPlayer() {
   const name = document.getElementById('nameInput')
   const chips = document.getElementById('chipsInput')
@@ -17,5 +18,39 @@ function addNewPlayer() {
 
   document.getElementById('playersList').appendChild(li) 
 }
-
 window.addNewPlayer = addNewPlayer
+
+function setUpTable() { 
+  const table = document.getElementById('players')
+  players.forEach((player, index) => {
+    const playerTile = document.createElement('div')
+    playerTile.setAttribute('id', `player-${index}`)
+    playerTile.setAttribute('style', 'display: inline-block')
+
+    const cardsTile = document.createElement('div')
+    cardsTile.setAttribute('id', `player-${index}-cards`)
+
+    const cardsImg = document.createElement('img')
+    cardsImg.setAttribute('href', '#')
+
+    const playerName = document.createTextNode(`  ${player.name}  `)
+    const playerChips = document.createTextNode(`  ${player.chips}  `)
+
+    cardsTile.appendChild(cardsImg)
+    cardsTile.appendChild(cardsImg)
+    
+    playerTile.appendChild(playerName)
+    playerTile.appendChild(playerChips)
+    playerTile.appendChild(cardsTile)
+
+    table.appendChild(playerTile)
+  })
+  document.getElementById('createGameForm').innerHTML = ''
+}
+
+function createBlackJackGame() {
+  setUpTable()
+  const game = new BlackJackGame(null, players)
+}
+
+window.createBlackJackGame = createBlackJackGame
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/PlayBlackJack.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
