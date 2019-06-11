aaf3b1d5d4305006f5eb245c49b65cf1a516030f
polynomiald <steven.grindey@gmail.com>
2019-06-11 12:48:21 +0100

    added some divs and images

--- src/PlayBlackJack.js
+++ src/PlayBlackJack.js
@@ -20,30 +20,47 @@ function addNewPlayer() {
 }
 window.addNewPlayer = addNewPlayer
 
-function setUpTable() { 
-  const table = document.getElementById('players')
+function setUpTable() {
+  const dealer = document.getElementById('dealer')
+  
+  const dealerImage = document.createElement('img')
+  dealerImage.setAttribute('src', '/home/steve/Pictures/dealer_avatar.png')
+  dealerImage.setAttribute('height', '50')
+  dealerImage.setAttribute('width', '50')
+  dealer.appendChild(dealerImage)
+
+  const dealerCardsTile = document.createElement('div')
+  dealerCardsTile.setAttribute('id', 'dealer-cards')
+  dealer.appendChild(dealerCardsTile)
+  
+  const playerRow = document.getElementById('players')
   players.forEach((player, index) => {
     const playerTile = document.createElement('div')
     playerTile.setAttribute('id', `player-${index}`)
     playerTile.setAttribute('style', 'display: inline-block')
+    
+    const playerImage = document.createElement('img')
+    playerImage.setAttribute('src', '/home/steve/Pictures/player_avatar.png')
+    playerImage.setAttribute('height', '50')
+    playerImage.setAttribute('width', '50')
 
     const cardsTile = document.createElement('div')
     cardsTile.setAttribute('id', `player-${index}-cards`)
 
-    const cardsImg = document.createElement('img')
-    cardsImg.setAttribute('href', '#')
+    const playerName = document.createElement('div')
+    playerName.setAttribute('id', `player${index}-name`)
+    playerName.innerHTML = player.name
 
-    const playerName = document.createTextNode(`  ${player.name}  `)
-    const playerChips = document.createTextNode(`  ${player.chips}  `)
+    const playerChips = document.createElement('div')
+    playerChips.setAttribute('id', `player${index}-chips`)
+    playerChips.innerHTML = player.chips
 
-    cardsTile.appendChild(cardsImg)
-    cardsTile.appendChild(cardsImg)
-    
+    playerTile.appendChild(playerImage)
     playerTile.appendChild(playerName)
     playerTile.appendChild(playerChips)
     playerTile.appendChild(cardsTile)
 
-    table.appendChild(playerTile)
+    playerRow.appendChild(playerTile)
   })
   document.getElementById('createGameForm').innerHTML = ''
 }
